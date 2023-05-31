55de65293ce47286ebea9df4f63689cc17db000b
mitsav01 <135125057+mitsav01@users.noreply.github.com>
2023-05-31 22:19:03 +0200

    helloworld.cpp
    Beginning of C++ learning

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+//This is my first C++ program
+
+#include<iostream>
+
+int main()
+{
+    std::cout << "Hello world! "  << '\n';
+    std::cout << "This is my first C++ program ";
+    return 0;
+}
+
+
+
